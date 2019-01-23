be3e6307cdb1c66bc739d3097bf15a01f5c880e0
mattshardman <mattshardman@gmail.com>
2019-01-23 14:29:58 +0000

    completed game stretch goal

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -20,6 +20,7 @@ function GameObject(assignments) {
 }
 
 GameObject.prototype.destroy = function() {
+  console.log(`${this.name} was removed from the game`);
   return `${this.name} was removed from the game`;
 };
 
@@ -38,7 +39,8 @@ function CharacterStats(assignments) {
 
 CharacterStats.prototype = { ...GameObject.prototype };
 
-CharacterStats.prototype.takeDamage = function() {
+CharacterStats.prototype.takeDamage = function(power) {
+  this.healthPoints -= power;
   return `${this.name} took damage`;
 };
 
@@ -60,9 +62,9 @@ function Humanoid(assignments) {
 
 Humanoid.prototype = { ...CharacterStats.prototype };
 
-Humanoid.prototype.greet = function () {
-  return `${this.name} offers a greeting in ${this.language}.`;
-}
+Humanoid.prototype.greet = function() {
+  return `${this.name}, of ${this.team} team, offers a greeting in ${this.language}.`;
+};
 
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -114,18 +116,123 @@ const archer = new Humanoid({
   language: "Elvish"
 });
 
-console.log(mage.createdAt); // Today's date
-console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-console.log(swordsman.healthPoints); // 15
-console.log(mage.name); // Bruce
-console.log(swordsman.team); // The Round Table
-console.log(mage.weapons); // Staff of Shamalama
-console.log(archer.language); // Elvish
-console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-console.log(mage.takeDamage()); // Bruce took damage.
-console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+// console.log(mage.createdAt); // Today's date
+// console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+// console.log(swordsman.healthPoints); // 15
+// console.log(mage.name); // Bruce
+// console.log(swordsman.team); // The Round Table
+// console.log(mage.weapons); // Staff of Shamalama
+// console.log(archer.language); // Elvish
+// console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+// console.log(mage.takeDamage()); // Bruce took damage.
+// console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 // Stretch task:
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+// attack method
+function attack (opponent, weapon) {
+  opponent.takeDamage(weapon.power);
+  return `${this.name} attacks with ${weapon.name}\n${opponent.name}'s health: ${opponent.healthPoints}`;
+};
+
+// villain constructor function
+function Villain(assignments) {
+  Humanoid.call(this, assignments);
+}
+
+Villain.prototype = { ...Humanoid.prototype, attack };
+
+// hero constructor function
+function Hero(assignments) {
+  Humanoid.call(this, assignments);
+}
+
+Hero.prototype = { ...Humanoid.prototype, attack };
+
+// generate random number
+const randNum = (num) => Math.floor(Math.random() * Math.floor(num));
+
+// hero vs villain fight function
+function createCharactersAndFight(villainObj, heroObj) {
+  // create new hero and new villain with objects passed into fight function
+  const hero = new Hero(heroObj);
+  const villain = new Villain(villainObj);
+
+  // fighters greet each other
+  console.log(hero.greet());
+  console.log(villain.greet());
+  
+  //fight function 
+  function fight() {
+
+    // if both character have health above 0 a fight begins
+    if (hero.healthPoints > 0 && villain.healthPoints > 0) {
+      // a random number decides who attacks
+      if (randNum(2)) {
+        const choice = randNum(hero.weapons.length);
+        // the character attacks with a random weapon from their weapons array, each weapon has a set power
+        console.log(hero.attack(villain, hero.weapons[choice]));
+      } else {
+        const choice = randNum(villain.weapons.length);
+        console.log(villain.attack(hero, villain.weapons[choice]));
+      }
+      // the function recursively calls itself until one character is dead
+      return setTimeout(() => fight(), 2000);
+    }
+  
+    // calls the destroy function on the looser
+    if (hero.healthPoints <= 0) {
+      hero.destroy();
+    } else {
+      villain.destroy();
+    }
+    
+    return `Fight over`;
+  };
+
+  // fight commences 2 seconds after characters greet each other
+  this.setTimeout(() => fight(), 2000);
+};
+
+// create objects for villain and hero
+const villainObj = {
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 20,
+  name: "Lawrence",
+  team: "Forest Kingdom",
+  weapons: [
+    { name: "a punch", power: 1 },
+    { name: "a wad of cash", power: 2 },
+    { name: "a wet fish", power: 1 },
+  ],
+  language: "Elvish"
+};
+
+const heroObj = {
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 20,
+  name: "Matt",
+  team: "The Round Table",
+  weapons: [
+    { name: "a plastic Sword", power: 2 },
+    { name: "a huge punch to the face which misses", power: 0 },
+    { name: "a kick move", power: 1 },
+  ],
+  language: "Common Tongue"
+};
+
+// call fight function with villain and hero objects
+createCharactersAndFight(villainObj, heroObj);
